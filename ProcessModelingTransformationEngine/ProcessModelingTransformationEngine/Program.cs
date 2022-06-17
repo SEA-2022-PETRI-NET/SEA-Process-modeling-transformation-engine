0eb4c9d81dcbb253e8ff46e435b590b04fc40833
Martin-Max-Kristensen <gkq790@alumni.ku.dk>
2022-06-17 22:36:54 +0200

    Fixes CORS

--- ProcessModelingTransformationEngine/ProcessModelingTransformationEngine/Program.cs
+++ ProcessModelingTransformationEngine/ProcessModelingTransformationEngine/Program.cs
@@ -23,6 +23,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod()
+    .AllowAnyOrigin());
+
 app.UseAuthorization();
 
 app.MapControllers();
